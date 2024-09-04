6887d91f32eb0499b8db7154934fd0a760c85041
João Lucas <joaollas@hotmail.com>
2024-09-04 08:07:20 -0300

    d04/09/2024

--- 7. LittleSister'sEssay.py
+++ 7. LittleSister'sEssay.py
@@ -4,16 +4,16 @@ In this exercise you are helping your younger sister edit her paper for school. 
 
 You have four tasks to clean up and modify strings.
 
-Task 1:
+Task 1: Capitalize the title of the paper
  Any good paper needs a properly formatted title. Implement the function capitalize_title(<title>) which takes a title str as a parameter and capitalizes the first letter of each word. This function should return a str in title case.
 
-Task 2:
+Task 2: Check if each sentence ends with a period
  You want to make sure that the punctuation in the paper is perfect. Implement the function check_sentence_ending() that takes sentence as a parameter. This function should return a bool.
 
-Task 3: 
+Task 3: Clean up spacing
  To make the paper look professional, unnecessary spacing needs to be removed. Implement the function clean_up_spacing() that takes sentence as a parameter. The function should remove extra whitespace at both the beginning and the end of the sentence, returning a new, updated sentence str.
 
-Task 4:
+Task 4: Replace words with a synonym
  To make the paper even better, you can replace some of the adjectives with their synonyms. Write the function replace_word_choice() that takes sentence, old_word, and new_word as parameters. This function should replace all instances of the old_word with the new_word, and return a new str with the updated sentence.
 """
 
